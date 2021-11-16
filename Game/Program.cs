40fe127fb184b0d0f99ca369346010442093c40c
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2021-11-16 08:30:16 +0100

    Generace Random Čísla

--- Game/Program.cs
+++ Game/Program.cs
@@ -6,7 +6,16 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Quessing Game");
+            Random random = new Random();
+            int randomnumber;
+            while (true)
+            {
+                randomnumber = random.Next(1, 5);
+                Console.WriteLine(randomnumber);
+                Console.ReadLine();
+            
+            }
         }
     }
 }
