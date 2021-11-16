93d39622ebb4452d7245a71faeb15ca446b32379
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2021-11-16 09:23:56 +0100

    Funkční

--- Game/Program.cs
+++ Game/Program.cs
@@ -9,10 +9,32 @@ namespace Game
             Console.WriteLine("Quessing Game");
             Random random = new Random();
             int RandomNumber;
+            int Guess;
+            bool neuhodl = true;
             RandomNumber = random.Next(1, 101);
             Console.WriteLine("----------------");
-            Console.Write("Tipněte si číslo 1-100: ");
-            Console.ReadLine();
+            Console.WriteLine("I think number between 1-101 ");
+
+            while (neuhodl)
+            {
+                Console.Write("Guess number: ");
+                Guess = int.Parse(Console.ReadLine());
+                if (Guess > RandomNumber)
+                {
+                    Console.WriteLine("The number is lower");
+                }
+                else if (Guess < RandomNumber)
+                {
+                    Console.WriteLine("The number is bigger");
+                }
+                else
+                {
+                    Console.WriteLine("You´ve won");
+                    neuhodl = false;
+                }
+            }
+            
+
 
 
         }
